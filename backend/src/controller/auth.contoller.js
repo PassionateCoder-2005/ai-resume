d4fceef783d024a2565633c8623d3b0705a938b9
Adheesh Tripathi <adheesh112004@gmail.com>
2026-07-12 01:05:08 +0530

    feat: enhance logout functionality by clearing cookie with security options

--- backend/src/controller/auth.contoller.js
+++ backend/src/controller/auth.contoller.js
@@ -104,7 +104,11 @@ export const logout=async (req,res) => {
             token:token
         })
 
-        res.clearCookie("token")
+        res.clearCookie("token",{
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
         return res.status(200).json({
             message:"Logout successful"
         })
